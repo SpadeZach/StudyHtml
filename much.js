bc12eef079e60f4ea3b0b95d37b7a14c1fb0a255
zhaobo <405540984@qq.com>
2017-03-17 14:38:06 +0800

    cookie  js中级完

--- much.js
+++ much.js
@@ -115,8 +115,22 @@ function removeClass(obj,className){
 		}
 	}
 }
-
-
+/************** 设置cookie ******************/
+function setCookie(key, value, t) {
+	var oDate = new Date();
+	oDate.setDate( oDate.getDate() + t );
+	document.cookie = key + '=' + value + ';expires=' + oDate.toGMTString();
+}
+//移除cookie
+function getCookie(key) {
+	var arr1 = document.cookie.split('; ');
+	for (var i=0; i<arr1.length; i++) {
+		var arr2 = arr1[i].split('=');
+		if ( arr2[0] == key ) {
+			return decodeURI(arr2[1]);
+		}
+	}
+}
 
 
 
