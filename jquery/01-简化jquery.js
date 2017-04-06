515d40f1b3053cd6210aa87ff188430405ad6844
zhaobo <405540984@qq.com>
2017-04-06 17:33:12 +0800

    no message

---
+++ jquery/01-简化jquery.js
@@ -0,0 +1,8 @@
+（function()	{
+	//48 ~ 113行 定义了一些变量和函数 jqurey = function  () {}
+	//114~192行 jQuery对象添加一些方法 属性
+	//195~262行 extend jQuery继承方法
+	//264~510行 jQuery.extend扩展工具方法
+	//10237 行  window.jQuery = window.$ = jQuery; 对外提供接口
+
+}）(); //匿名函数自执行
